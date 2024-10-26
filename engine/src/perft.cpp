13703140155329b6e0cb40dba55f645599dc2cac
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2024-10-26 16:13:45 +0300

    Perft: only counting captures, e. p. and castles at the leaf nodes

--- engine/src/perft.cpp
+++ engine/src/perft.cpp
@@ -19,33 +19,40 @@ static void perft(Board& board, size_t depth, PerftResults& results, const Perft
 		Board oldBoard = board;
 		if (board.applyMove(move))
 		{
-			// Detect castling moves
-			if (board.pieceAt(move.to()).type() == King &&
-				(
-					(move.from() == toSquare(0, 4) && move.to() == toSquare(0, 6)) ||
-					(move.from() == toSquare(7, 4) && move.to() == toSquare(7, 6)) ||
-					(move.from() == toSquare(0, 4) && move.to() == toSquare(0, 2)) ||
-					(move.from() == toSquare(7, 4) && move.to() == toSquare(7, 2))
-				)) [[unlikely]]
+			if (depth == 1)
 			{
-				results.castling += 1;
-			}
-			else
-			{
-				// Detect en passant moves
-				if (board.pieceAt(move.to()).type() == Pawn && move.isCapture() && oldBoard.pieceAt(move.to()).type() == EmptySquare) [[unlikely]]
-					results.enPassant += 1;
-
-				results.captures += (uint64_t)move.isCapture();
-			}
-
-			if (depth > 1)
-				perft(board, depth - 1, results, printFunc, false);
-			else
 				results.nodes += 1;
 
+				// Detect castling moves
+				if (board.pieceAt(move.to()).type() == King &&
+					(
+						(move.from() == toSquare(0, 4) && move.to() == toSquare(0, 6)) ||
+						(move.from() == toSquare(7, 4) && move.to() == toSquare(7, 6)) ||
+						(move.from() == toSquare(0, 4) && move.to() == toSquare(0, 2)) ||
+						(move.from() == toSquare(7, 4) && move.to() == toSquare(7, 2))
+						)) [[unlikely]]
+				{
+					results.castling += 1;
+				}
+				else
+				{
+					// Detect en passant moves
+					if (board.pieceAt(move.to()).type() == Pawn && move.isCapture() && oldBoard.pieceAt(move.to()).type() == EmptySquare) [[unlikely]]
+						results.enPassant += 1;
+
+						results.captures += (uint64_t)move.isCapture();
+				}
+			}
+			else
+			{
+				perft(board, depth - 1, results, printFunc, false);
+			}
+
 			if (print && printFunc) [[unlikely]]
+			{
 				printFunc(move.notation(), results.nodes - prevNodesCount);
+				//std::cout << "Duplicates: " << duplicates << std::endl;
+			}
 		}
 
 		board = oldBoard;
