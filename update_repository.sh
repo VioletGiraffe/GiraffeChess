056e49d65db91bf1e45bc69fc7f305c93d64ed55
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2024-12-14 14:09:40 +0200

    update_repository.sh

--- update_repository.sh
+++ update_repository.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # Update the main repo
-git checkout master
+git checkout main
 git fetch
 git pull
 
 # Init the subrepos
 git submodule update --init --recursive
 git submodule foreach --recursive "git checkout master"
+git submodule foreach --recursive "git checkout main"
 
 # Update the subrepos
 git submodule foreach --recursive "git pull"
